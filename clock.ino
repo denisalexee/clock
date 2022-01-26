d2d4fd2ba0b2f511ecbc82a4efcf8969e4af14de
Denis <denisalexee86@gmail.com>
2022-01-26 12:50:49 +0300

    display to text

--- clock.ino
+++ clock.ino
@@ -1,15 +1,24 @@
 #include <Wire.h>
 #include "SparkFunHTU21D.h"
-#include "MAX30105.h"  
-#include "BluetoothSerial.h"
-#include <GyverOLED.h>
+#include "MAX30105.h"           // Библиотека пульсометра
+#include "BluetoothSerial.h"    // Библиотека обычного Bluetooth
+#include <GyverOLED.h>          // Библиотека экрана
+#include <Rtc_Pcf8563.h>        // Библиотека часов реального времени
+#include "MPU6050.h"            // Библиотека гироскоп
+#include "heartRate.h"
+#include "spo2_algorithm.h"  
+#include <FS.h>                // Доступ к файловой системе
+#include <SPIFFS.h>
+//----------------bitmap--------------------------
+#include "Still.h"
+#include "batt.h"
 
 MAX30105 particleSensor;
 HTU21D myHumidity;
 BluetoothSerial ESP_BT;
 GyverOLED<SSH1106_128x64> oled;
 
-char Lorem_ipsum[] = "Lorem ipsum dolor sit amet, лорем ипсум долор сит амет привет народ ё, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip...";
+
 const int RES = 27;
 
 #ifdef __cplusplus
@@ -24,21 +33,28 @@ uint8_t temprature_sens_read();
 
 uint8_t temprature_sens_read();
 
+
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(115200); 
+  Serial.println("Battary_low");
+  oled.init();
+  oled.clear();
+  oled.drawBitmap(0,0,batt_low,32,32);
+  oled.update();//, sizeof(Still_one)
+  delay(10000);
   ESP_BT.begin("ESP32_Temper");
   Serial.println("HTU21D Example!");
-  pinMode(RES, OUTPUT);
-  digitalWrite(RES,HIGH);
-  delay(1000); 
-  digitalWrite(RES, LOW);
-  //             инициализация
-  digitalWrite(RES, LOW);
-  delay(1000);
-  digitalWrite(RES,HIGH);  
+  // pinMode(RES, OUTPUT);
+  // digitalWrite(RES,HIGH);
+  // delay(1000); 
+  // digitalWrite(RES, LOW);
+  // //             инициализация
+  // digitalWrite(RES, LOW);
+  // delay(1000);
+  // digitalWrite(RES,HIGH);  
 
-  oled.init();  
+   
   myHumidity.begin();
 }
 
@@ -95,10 +111,10 @@ void printTest(const float& t,const float& h, const float& cpu ) {
   oled.println(h);
    oled.print(data2);
   oled.println(cpu);
-  oled.invertText(true);
+  //oled.invertText(true);
   //oled.println(data2);
   //oled.println(data3);
-  oled.invertText(false);
+  //oled.invertText(false);
   oled.update();
   //delay(5000);
 }
